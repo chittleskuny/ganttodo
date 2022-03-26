c0ff7546ff74aa9865d2713326617c3062950c7e
Chittle Skuny <chittleskuny@gmail.com>
2022-03-26 18:22:13 +0800

    The install worked successfully! Congratulations!

--- ganttodo/settings.py
+++ ganttodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4l-#*_po9vpqowsuqajpd%mip4j0ig26@)gddt6o-5hy&$7c-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'ganttodo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'ganttodo',
+        },
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
